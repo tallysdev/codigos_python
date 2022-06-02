286f37d56a39b2d523d201df90aa557fbf5d86a0
Tallys-Aureliano <tallysaureliano@outlook.com>
2022-06-02 15:38:50 -0300

    nova atividade

---
+++ calor_lista.py
@@ -0,0 +1,11 @@
+# Escreva um programa em Python que leia e
+# armazene 6 valores de temperatura, obtidos
+# a partir do teclado. Em seguida, o programa
+# deve exibir na tela a lista de temperaturas nas
+# seguintes formas:
+# a) Na mesma ordem em que foram lidos do
+# teclado
+# b) Na ordem inversa à que foram lidos do
+# teclado
+# c) Ordenados em ordem crescente
+# d) Ordenados em ordem decrescente
