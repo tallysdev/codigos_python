de1743a9920ec3babd88f50c67fcb99f6fa9cfea
Tallys-Aureliano <tallysaureliano@outlook.com>
2022-06-02 17:24:51 -0300

    metodo que eu usei para resolver o problema

--- media_termo.py
+++ media_termo.py
@@ -3,14 +3,38 @@
 # paciente internado ao longo de um
 # determinado dia. As medições devem ser
 # feitas a cada três horas.
+
 # A seguir, calcule a temperatura média do dia
 # e exiba esse valor na tela.
+
 # Por fim, verifique e indique quantas vezes e
 # em quais horários a temperatura ficou acima
 # da temperatura média do dia.
 
 tempe = []
+maiores = []
+media = 0
+t = 0
+h = []
+
 for i in range(8):
-    ratura = input('Informe a temperatura')
+    t += 1
+    print('informe a temperatura', t)
+    ratura = float(input())
     tempe.append(ratura)
-print(tempe)
+
+print(tempe,'\n')
+
+for i in range(8):
+    media = media + tempe[i]
+
+mediaf = media/8
+
+print(mediaf,'\n')
+
+for i in range(8):
+    if mediaf < tempe[i]:
+        maiores.append(tempe[i])
+        h.append(i)
+
+print('as temperaturas maiores foram', sorted(maiores),'nos horarios', sorted(h))
