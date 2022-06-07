e9387200d23a0b4af00e5eb16ca6cb0a71a55200
Tallys-Aureliano <tallysaureliano@outlook.com>
2022-06-07 17:15:59 -0300

    botando a mao na massa

--- jogo_da_velha.py
+++ jogo_da_velha.py
@@ -1,3 +1,6 @@
+from random import randint
+
+
 tabu = [
  ['_', '_', '_'],
  ['_', '_', '_'],
@@ -30,13 +33,14 @@ for i in range(3):
 print()
 
 ganhou = False
-while jogada < 10 and not(ganhou):
+jogadas = 0
+while jogadas < 9 and not(ganhou):
 
     jog = int(input("Informe a posição da sua jogada"))
     i = (jog-1) // 3
     j = (jog-1) % 3
     tabu[i][j] = 'O'
-
+    jogadas+=1
     print("Tabuleiro oficial")
     for i in range(3):
         for j in range(3):
@@ -44,6 +48,27 @@ while jogada < 10 and not(ganhou):
  
         print()
     print()
+
+    print('Jogada do computador')
+    achou = False
+    while not achou:
+        jcomp = randint(0,8)
+        i = (jcomp) // 3
+        j = (jcomp) % 3
+        if tabu[i][j]=='_':
+            tabu[i][j] = 'X'
+            achou = True
+            jogadas+=1
+
+    
+    print("Tabuleiro oficial")
+    for i in range(3):
+        for j in range(3):
+            print(tabu[i][j], end=' ')
+ 
+        print()
+    print()
+
 
     if (tabu[0][0] == tabu[0][1]) and (tabu[0][1] == tabu[0][2]) and (tabu[0][2] != '_'):
         ganhou = True
@@ -62,5 +87,6 @@ while jogada < 10 and not(ganhou):
     elif (tabu[0][2] == tabu[1][1]) and (tabu[1][1] == tabu[2][0]) and (tabu[2][0] != '_'):
         ganhou = True
 
-
+if jogadas == 8:
+    print('Empateeee')
 
