69748e311696a4e1020694d8749071e50f076f4e
Tallys-Aureliano <tallysaureliano@outlook.com>
2022-06-07 16:10:03 -0300

    carregando codigos adcionais

--- matrizes.py
+++ matrizes.py
@@ -1,11 +1,29 @@
-matriz1 = []
-matriz2 = [[1, 2], [3, 4]]
-matriz3 = [
-[1, 2, 3],
-[4, 5, 6],
-[7, 8, 9]
- ]
+# matriz1 = []
+# matriz2 = [[1, 2], [3, 4]]
+# matriz3 = [
+# [1, 2, 3],
+# [4, 5, 6],
+# [7, 8, 9]
+#  ]
 
-print(matriz1,'\n')
-print(matriz2,'\n')
-print(matriz3,'\n')
+# print(matriz1,'\n')
+# print(matriz2,'\n')
+# print(matriz3,'\n')
+
+print('Programa Matrizes')
+print('Informe as dimensões da matriz')
+m = int(input('Quantas linhas? '))
+n = int(input('Quantas colunas? '))
+mat = []
+for i in range(m):
+    mat.append([])
+    for j in range(n):
+        v = int(input('M[%d][%d]: '%(i+1,j+1)))
+        mat[i].append(v)
+print()
+print('Matriz lida')
+for i in range(m):
+    print('|', end='')
+    for j in range(n):
+        print('%3d'%mat[i][j], end='')
+    print(' |')
