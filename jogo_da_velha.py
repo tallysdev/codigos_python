206e7954e7b315627e4d0a7d9b0e5fba280435e1
Tallys-Aureliano <tallysaureliano@outlook.com>
2022-06-07 16:21:37 -0300

    esboço do projeto

--- jogo_da_velha.py
+++ jogo_da_velha.py
@@ -1,1 +1,24 @@
-jogo_da_velha
+velha = [
+ ['_', '_', '_'],
+ ['_', '_', '_'],
+ ['_', '_', '_']
+ ]
+print("Tabuleiro")
+for i in range(3):
+    for j in range(3):
+        print(velha[i][j], end=' ')
+ 
+    print()
+print()
+posicoes = [
+ ['1', '2', '3'],
+ ['4', '5', '6'],
+ ['7', '8', '9']
+ ]
+print("posiçoes do Tabuleiro")
+for i in range(3):
+    for j in range(3):
+        print(posicoes[i][j], end=' ')
+ 
+    print()
+print()
