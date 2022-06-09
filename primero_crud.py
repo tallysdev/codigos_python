116a99468ae6c3e5fc7e9668ab0016a0f8b1fad7
Tallys-Aureliano <tallysaureliano@outlook.com>
2022-06-09 15:58:17 -0300

    trocando de lista para matrix

--- primero_crud.py
+++ primero_crud.py
@@ -1,6 +1,7 @@
 # Agenda de contatos
 
 go = input('Quer ver sua agenda de contatos?\t(s/n)\n')
+agenda = []
 contatos = []
 while go == 's':
     print('##########################')
@@ -11,16 +12,19 @@ while go == 's':
     print('## 4 - Apagar contato    ##')
     print('## 5 - Listar todos      ##')
     print('## 0 - Sair              ##')
-    opcao = input()
+    opcao = input('\n')
     # sair
     if opcao == '0':
         go = 'xau'
     elif opcao == '1':
-        n = input('deseja adicionar quantos contatos?\n')
+        n = int(input('deseja adicionar quantos contatos?\n'))
         for i in range(n):
-            contato = input('Informe o contato\n', (i+1))
-            contatos.append(contato)
+            contato = input('Informe o nome do contato\n', (i+1))
+            contatos.append(agenda)
+            email = input('Informe o email do ccontato\n', (i+1))
+            contatos.append(agenda)
+            num = input('Informe o numero do contato\n', (i+1))
+            contatos.append(agenda)
         print(contatos)
-
 
 print('Voce saiu da agenda')
