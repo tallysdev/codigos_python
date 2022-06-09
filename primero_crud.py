9cedc3caa51ab2af0a663baa33821c12f02ff94b
Tallys-Aureliano <tallysaureliano@outlook.com>
2022-06-09 15:29:14 -0300

    corrigindo opcoes

--- primero_crud.py
+++ primero_crud.py
@@ -5,18 +5,18 @@ contatos = []
 while go == 's':
     print('##########################')
     print('### Agenda de contatos ###')
-    print('## 1- Cadastrar usuario ##')
-    print('## 2- Pesquisar contato ##')
-    print('## 3- Atualizar contato ##')
-    print('## 4- Apagar contato    ##')
-    print('## 5- Listar todos      ##')
-    print('## 0- Sair              ##')
+    print('## 1 - Cadastrar contato ##')
+    print('## 2 - Pesquisar contato ##')
+    print('## 3 - Atualizar contato ##')
+    print('## 4 - Apagar contato    ##')
+    print('## 5 - Listar todos      ##')
+    print('## 0 - Sair              ##')
     opcao = input()
     # sair
     if opcao == '0':
         go = 'xau'
     elif opcao == '1':
-        n = int(input('deseja adicionar quantos contatos?\n'))
+        n = input('deseja adicionar quantos contatos?\n'))
         for i in range(n):
             contato = input('Informe o contato\n')
             contatos.append(contato)
