82b01e44e9d064af7b18fabfe3d3d10de3b9f1b6
Tallys-Aureliano <tallysaureliano@outlook.com>
2022-06-13 18:12:54 -0300

    adcionando texto base da questão 1

--- exercicius_de_revisao/questao_1.py
+++ exercicius_de_revisao/questao_1.py
@@ -1,3 +1,9 @@
+# 1. O programa em Python a seguir simula um jogo de dados entre um jogador humano e o
+# computador:
+# Modifique o programa anterior de forma que possam ser disputadas várias partidas, até que
+# um dos jogadores sagre-se vencedor. Em cada caso, identifique a estrutura de repetição e o
+# critério de parada mais adequado para resolver o problema:
+
 import random
 jog = random.randint(1,6)
 comp = random.randint(1,6)
