f8c6e2a30316225eb95e08ec7c0af70758aa8afa
Tallys-Aureliano <tallysaureliano@outlook.com>
2022-06-13 17:54:28 -0300

    adcionando minha forma de desolvolver a B questao

--- exercicius_de_revisao/N1_b).py
+++ exercicius_de_revisao/N1_b).py
@@ -1,12 +1,18 @@
 import random
-jog = random.randint(1,6)
-comp = random.randint(1,6)
-print('Jogador: ', jog)
-print('Computador: ',comp)
-if (jog > comp):
-    print('Parabens, voce ganhou!')
-elif(comp > jog):
-    print('Infelizmente, o computador')
-else:
-    print('Jogo Empatado!!!')
+vjog = 0 
+vcomp = 0
+while vjog < 6 or vcomp < 6:
+    jog = random.randint(1,6)
+    comp = random.randint(1,6)
+    print('Jogador: ', jog)
+    print('Computador: ',comp)
+    if (jog > comp):
+        vjog+= 1
+        print('Parabens, voce ganhou!')
+    elif(comp > jog):
+        vcomp+=1
+        print('Infelizmente, o computador')
+    else:
+        print('Jogo Empatado!!!')
 print('Fim do Programa')
+print('O jogador ganhou:',vjog,'e o computador ganhou',vcomp)
