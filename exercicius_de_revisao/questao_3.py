8e289c492924502dd246ccc317ebe01a74a9bc1b
Tallys-Aureliano <tallysaureliano@outlook.com>
2022-06-13 18:15:42 -0300

    adcionando texto base da questão 3

--- exercicius_de_revisao/questao_3.py
+++ exercicius_de_revisao/questao_3.py
@@ -1,3 +1,8 @@
+# 3. Escreva um programa em Python que gere e exiba a sequência de números abaixo:
+# 1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5, 6, ...
+# ou seja, cada i-ésimo elemento da sequência aparece i vezes repetido na lista. O valor de i
+# deve ser informado pelo informado pelo usuário no início da execução do programa
+
 i = 0
 for i in range(7):
     for j in range(i):
