b6946aa2a4925aa91aec320dcacc241fce87e370
Tallys-Aureliano <tallysaureliano@outlook.com>
2022-06-13 17:27:49 -0300

    adicioanando minha resulução da letra A do questao 1

--- exercicius_de_revisao/N1_a).py
+++ exercicius_de_revisao/N1_a).py
@@ -1,12 +1,23 @@
 import random
-jog = random.randint(1,6)
-comp = random.randint(1,6)
-print('Jogador: ', jog)
-print('Computador: ',comp)
-if (jog > comp):
-    print('Parabens, voce ganhou!')
-elif(comp > jog):
-    print('Infelizmente, o computador')
-else:
-    print('Jogo Empatado!!!')
+part = 0
+vjog = 0
+vcomp = 0
+while part < 4:
+    jog = random.randint(1,6)
+    comp = random.randint(1,6)
+    print('Jogador: ', jog)
+    print('Computador: ',comp)
+    if (jog > comp):
+        print('Parabens, voce ganhou!')
+        part = part + 1
+        vjog = vjog + 1
+    elif(comp > jog):
+        print('Infelizmente, o computador ganhou')
+        part = part + 1
+        vcomp = vcomp + 1
+    else:
+        print('Jogo Empatado!!!')
+        part = part + 1
+        
 print('Fim do Programa')
+print('O jogador ganhou:',vjog,'e o computador ganhou',vcomp)
