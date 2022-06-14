1fb7e4360dd4d687b6750ce27db6d3954059340c
Tallys-Aureliano <tallysaureliano@outlook.com>
2022-06-13 21:46:14 -0300

    adicioando questão 2

---
+++ exercicius_de_revisao/Questao_2.py
@@ -0,0 +1,5 @@
+# 2. A amplitude de um conjunto de dados é uma medida estatística de dispersão definida como
+# sendo a diferença entre o maior e o menor dentre os valores existentes nesse conjunto de
+# dados. Escreva um programa em Python que leia uma sequência de k valores inteiros e
+# armazene-os em uma lista. Em seguida, o programa deve calcular e exibir a amplitude desse
+# conjunto de dados. O valor de k deve ser definido pelo usuário no início do programa.
