cd897e085785e7f56621d63023f51373d98d3a7b
Tallys-Aureliano <tallysaureliano@outlook.com>
2022-06-24 18:44:17 -0300

    main: voltando tudo como estava antes pq era a melhor forma

--- primero_crud.py
+++ primero_crud.py
@@ -2,9 +2,7 @@
 
 go = input('Quer ver sua agenda de contatos?\t(s/n)\n')
 agenda = []
-tell = []
-email = []
-nome = []
+contatos = []
 while go == 's':
     print('##########################')
     print('### Agenda de contatos ###')
@@ -24,15 +22,12 @@ while go == 's':
         n = int(input('deseja adicionar quantos contatos?\n'))
         for i in range(n):
             cont = input('Informe o nome do contato {} \n' .format(i+1))
-            nome.append(cont)
-            emai = input('Informe o email do contato {} \n' .format(i+1))
-            email.append(email)
+            contatos.append(cont)
+            email = input('Informe o email do contato {} \n' .format(i+1))
+            contatos.append(email)
             num = input('Informe o numero do contato {} \n' .format(i+1))
-            tell.append(num)
-
-            agenda.append(nome)
-            agenda.append(tell)
-            agenda.append(email)
+            contatos.append(num)
+            agenda.append(contatos)
 
     elif opcao == '2':
         print()
