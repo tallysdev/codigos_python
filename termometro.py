a7f9916568af196e56e417ea546de6b83a1726ad
Tallys-Aureliano <tallysaureliano@outlook.com>
2022-06-27 14:54:56 -0300

    main: add termometro

---
+++ termometro.py
@@ -0,0 +1,2 @@
+# Escreva um programa em Python que gere valores aleatórios de temperatura simulando leituras em um termômetro a cada hora ao longo de um dia. Os valores gerados devem seguir aproximadamente os valores reais de temperatura em sua localidade. Por exemplo, em uma cidade do interior do RN, valores típicos de temperatura variam entre 15°C e 45ºC, decaindo na madrugada até o nascer do sol, depois aumentando até as 15h aproximadamente e, em seguida, decaindo novamente até a meia-noite.
+
