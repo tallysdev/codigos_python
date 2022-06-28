cf7db9392a656f84a434af766d086e241e83a627
Tallys-Aureliano <tallysaureliano@outlook.com>
2022-06-28 15:16:53 -0300

    main: att forma de pegar temperaturas

--- termometro.py
+++ termometro.py
@@ -1,22 +1,31 @@
 # Escreva um programa em Python que gere valores aleatórios de temperatura simulando leituras em um termômetro a cada hora ao longo de um dia. Os valores gerados devem seguir aproximadamente os valores reais de temperatura em sua localidade. Por exemplo, em uma cidade do interior do RN, valores típicos de temperatura variam entre 15°C e 45ºC, decaindo na madrugada até o nascer do sol, depois aumentando até as 15h aproximadamente e, em seguida, decaindo novamente até a meia-noite.
 
-from random import randint
-from tkinter import W
+from random import randint, random, uniform
 
 
 temp = []
 aux = 0
+saia = False
 for hora in range(24):
-    c = randint(15,45)
-    while c > 25 and hora < 10 or c > 35 and hora > 16:
-        c = randint(15,45)
+    c = uniform(15,45)
+    while c > 25 and hora < 9 or c > 30 and hora > 20:
+        c = uniform(15,45)
+    
     temp.append(c)
-    tam = len (temp)
+    tam = len(temp)
+    
     if tam > 1:
-        aux = tam-1
+        aux = tam-2
+            
+    # print(c)
+    # print(temp[aux])
+    # print(aux)
+    # print(tam)
 
-    while c - temp[aux] > 4 or c - temp[aux] < -4:
-        c = randint(15,45)
-
-    print('as temperaturas em {}h foram: {}' .format(hora,c))
-print(temp)
+    while (((c - temp[aux] > 4 or c - temp[aux] < -4) and (hora < 10 or hora >18) and saia == False)):
+        c = uniform(15,45)
+        print('entrou')
+        saia == True
+    temp.append(c)
+    
+    print('as temperaturas em %d h foram: %.2f, ' %(hora,c))
