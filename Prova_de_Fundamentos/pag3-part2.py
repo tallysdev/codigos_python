ef10f5a0defc4a33c206abc1ef3518597e08615c
Tallys-Aureliano <tallysaureliano@outlook.com>
2022-07-03 18:42:17 -0300

    mian: add questao 22 resolvda

--- Prova_de_Fundamentos/pag3-part2.py
+++ Prova_de_Fundamentos/pag3-part2.py
@@ -2,32 +2,32 @@ from math import sqrt
 #questão 22
 #Ln=L(n-1)+L(n-2) L1=1 L2=3 n>=3
 
-recursaoq22=int(input("Digite um recursaoq22:"))
+recursao22=int(input("Digite um valor:"))
 
-def funcao(recursaoq22):
-  if recursaoq22==1:
+def recursaoQ22(recursao22):
+  if recursao22==1:
     return 1
-  elif recursaoq22==2:
+  elif recursao22==2:
     return 3
   else:
-    return funcao(recursaoq22-1) + funcao(recursaoq22-2)
+    return recursaoQ22(recursao22-1) + recursaoQ22(recursao22-2)
 
-print(funcao(recursaoq22))
+print('Usando a Recursão:',recursaoQ22(recursao22))
 
-a1=1
-a0=3
-resultado=1
-if recursaoq22==2:
-  resultado=a0
+a1_22=1
+a0_22=3
+resultado_22=1
+if recursao22==2:
+  resultado_22=a0_22
 else:
-  for i in range(3,recursaoq22+1):
-      resultado=a1+a0
-      a1=a0
-      a0=resultado
+  for i in range(3,recursao22+1):
+      resultado=a1_22+a0_22
+      a1_22=a0_22
+      a0_22=resultado_22
 
-print(resultado)
+print('Por loop:',resultado_22)
 
 def recursao(n):
   return (((1+sqrt(5))/2)**n)+(((1-sqrt(5))/2)**n)
 
-print("%.2f"%recursao(recursaoq22))
+print('Por Solução:',"%.2f"%recursao(recursao22))
