5c82dda71b42a35c845ccd8d8906031d84ab5b13
Tallys-Aureliano <tallysaureliano@outlook.com>
2022-07-03 18:11:30 -0300

    main:add minha forma de resolver a pag 3

--- Prova_de_Fundamentos/pag3.py
+++ Prova_de_Fundamentos/pag3.py
@@ -1,7 +1,7 @@
 #questão 15
 #An=6A(n-1)-8A(n-2) a0=1 a1=0
 
-recursao15=int(input("informe o valor de n para todas a questões:"))
+recursao15=int(input("informe o recursao17 de n para todas a questões:"))
 
 def recursaoQ15(recursao15):
   if recursao15==0:
@@ -52,16 +52,16 @@ def recursaoQ16(recursao16):
 print('\nQuestão 16:\n')
 print('Usando a Recursão:',recursaoQ16(recursao16))
 
-valor0=4
-valor1=10
+recursao170=4
+recursao171=10
 resultado=4
 if recursao16==1:
   resultado=10
 else:
   for i in range(2,recursao16+1):
-    resultado=2*valor1+8*valor0
-    valor0=valor1
-    valor1=resultado
+    resultado=2*recursao171+8*recursao170
+    recursao170=recursao171
+    recursao171=resultado
 
 print('Por loop:',resultado)
 
@@ -71,3 +71,40 @@ def solucao(n):
 print('Por Solução:', solucao(recursao16))
 
 #######################################################################
+
+#questão 17
+#An=7A(n-1)-10A(n-2) a0=5 a1=16
+
+recursao17 = recursao15
+
+def recursaoQ17(recursao17):
+  if recursao17==0:
+    return 5
+  elif recursao17==1:
+    return 16
+  else:
+    return 7*recursaoQ17(recursao17-1)-10*recursaoQ17(recursao17-2)
+
+print('\nQuestão 17:\n')
+
+print('Usando a Recursão:',recursaoQ17(recursao17))
+
+a0=5
+a1=16
+resultado=5
+if recursao17==1:
+  resultado=16
+else:
+  for i in range(2,recursao17+1):
+    resultado=7*a1-10*a0
+    a0=a1
+    a1=resultado
+
+print('Por Loop:',resultado)
+
+def solucao(n):
+  return 2*(5**n)+3*(2**n)
+
+print('Por Solução:',solucao(recursao17))
+
+#####################################################################
