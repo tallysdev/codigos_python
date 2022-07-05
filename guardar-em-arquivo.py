36fb5d1499baa97d4dbbd9a92c127bfd4d7ff0a1
Tallys-Aureliano <tallysaureliano@outlook.com>
2022-07-05 16:57:56 -0300

    main: alterando para gerar em ordem alfabetica

--- guardar-em-arquivo.py
+++ guardar-em-arquivo.py
@@ -2,5 +2,24 @@ lista = ['Gol', 'Uno', 'Palio',
 'EcoSport', 'Clio', 'Strada', 'Golf']
 arq = open('carros.txt', 'w')
 for carro in lista:
- arq.write(carro + '\n')
+    arq.write(carro + '\n')
 arq.close()
+
+
+lista2 = []
+arq = open('carros.txt', 'r')
+for linha in arq:
+    linha = linha.rstrip()
+    lista2.append(linha)
+arq.close()
+
+arq = open('carros.txt','a')
+
+arq.sort()
+
+arq.close()
+
+lista2.sort()
+print('Lista de carros do arquivo:')
+for carro in lista2:
+ print(carro)
